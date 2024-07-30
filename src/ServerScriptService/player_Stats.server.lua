ff4bb1b8da5e5e8cf8621ba404c280ed4f23bcab
Pruninho <arthurhenrique41@gmail.com>
2024-07-30 11:34:11 -0300

    started!

---
+++ src/ServerScriptService/player_Stats.server.lua
@@ -0,0 +1,26 @@
+local starterPlayer = game:GetService("StarterPlayer")
+local Players = game:GetService("Players")
+
+function statsCreate(player)
+    local leaderStats = Instance.new("Folder")
+    --configs
+          leaderStats.Name = "leaderstats"
+          leaderStats.Parent = player
+
+    local Level = Instance.new("IntValue")
+    --configs
+          Level.Name = "Level"
+          Level.Parent = leaderStats
+
+    local toonCredits = Instance.new("IntValue")
+    --configs
+          toonCredits.Name = "toonCredits"  
+          toonCredits.Parent = leaderStats
+
+    local toonMoney = Instance.new("IntValue")
+          --configs
+          toonMoney.Name = "toonMoney"  
+          toonMoney.Parent = leaderStats
+end
+
+Players.PlayerAdded:Connect(statsCreate)
