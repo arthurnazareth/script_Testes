262a155b1229752cd79efc2f81b25499673f03fd
Pruninho <arthurhenrique41@gmail.com>
2024-07-31 01:40:40 -0300

    agarrando

--- src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
+++ src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
@@ -4,8 +4,20 @@ local job = job_Folder:FindFirstChild("charging_Assistant#job")
 local all_box = job:FindFirstChild("boxs")
 local box = all_box:GetChildren()
 
+local animation = script.Carry
+
 isCarrying = false
 box_Color = nil
+
+local function disableAllPrompts()
+    for _, boxs in ipairs(box) do
+        for _, child in ipairs(boxs:GetChildren()) do
+            if child:IsA("ProximityPrompt") then
+                child.Enabled = false
+            end
+        end
+    end
+end
 
 for _, boxs in ipairs(box) do
 	for _, proximity in ipairs(boxs:GetChildren()) do
@@ -14,25 +26,36 @@ for _, boxs in ipairs(box) do
 				isCarrying = true
 				if isCarrying then
 					local character = player.character
+					local humanoid = character:FindFirstChildOfClass("Humanoid")
 					local box = proximity.Parent
-					local weld = Instance.new("Motor6D", character.HumanoidRootPart)
-					weld.Part0 = box
-					weld.Part1 = character.HumanoidRootPart
-					weld.Name = "box_Weld"
-	
 					--configs
 					box.CanCollide = false
 					box.Anchored = false
-	
+
+					local prompt = box.ProximityPrompt
+
+					animTrack = humanoid:LoadAnimation(animation)
+					animTrack:Play()
+
+
+
+					local weld = Instance.new("Motor6D", character.HumanoidRootPart)
+					--configs
+					weld.Part0 = box
+					weld.Part1 = character.HumanoidRootPart
+
+					weld.C0 = CFrame.new(0, 0.3, 1.6)
+
+					weld.Name = "box_Weld"
+
 					box_Color = box:FindFirstChild("box_Color").Value
-					proximity.Enabled = false
+					disableAllPrompts()
 				end
-			end
-			if isCarrying then
-				proximity.Enabled = false
 			end
 		end
 	end
 	boxs:WaitForChild("ProximityPrompt").Triggered:Connect(proximityBox)
 end
+	
+
 
