ae3f4c11ddab18f1cc060d0349a2e4b42bd4c28c
Pruninho <arthurhenrique41@gmail.com>
2024-07-31 01:07:55 -0300

    update#2

--- src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
+++ src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
@@ -4,15 +4,35 @@ local job = job_Folder:FindFirstChild("charging_Assistant#job")
 local all_box = job:FindFirstChild("boxs")
 local box = all_box:GetChildren()
 
+isCarrying = false
+box_Color = nil
+
 for _, boxs in ipairs(box) do
-	box = boxs
-	print(box)
+	for _, proximity in ipairs(boxs:GetChildren()) do
+		if proximity:IsA("ProximityPrompt") then
+			function proximityBox(player)
+				isCarrying = true
+				if isCarrying then
+					local character = player.character
+					local box = proximity.Parent
+					local weld = Instance.new("Motor6D", character.HumanoidRootPart)
+					weld.Part0 = box
+					weld.Part1 = character.HumanoidRootPart
+					weld.Name = "box_Weld"
+	
+					--configs
+					box.CanCollide = false
+					box.Anchored = false
+	
+					box_Color = box:FindFirstChild("box_Color").Value
+					proximity.Enabled = false
+				end
+			end
+			if isCarrying then
+				proximity.Enabled = false
+			end
+		end
+	end
+	boxs:WaitForChild("ProximityPrompt").Triggered:Connect(proximityBox)
 end
-
-function proximityBox(player)
-end
-
-
-
-
 
