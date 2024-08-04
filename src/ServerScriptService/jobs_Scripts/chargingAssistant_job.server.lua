c593b19e2d823c8a2c4bea56df98601a7182e097
Pruninho <arthurhenrique41@gmail.com>
2024-08-04 03:16:31 -0300

    super update

--- src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
+++ src/ServerScriptService/jobs_Scripts/chargingAssistant_job.server.lua
@@ -1,3 +1,4 @@
+local ContentProvider = game:GetService("ContentProvider")
 local job_Folder = game.Workspace:WaitForChild("jobs_Folder")
 local job = job_Folder:FindFirstChild("charging_Assistant#job")
 
@@ -9,14 +10,99 @@ local animation = script.Carry
 isCarrying = false
 box_Color = nil
 
+old_box = nil
+local cooldown = 5
+
+local function cooldown_Box(timer)
+	local cooldown_GUI = old_box.Timer
+	local cooldown_Value = cooldown_GUI.timer_Value
+
+	cooldown_Value.Value = cooldown
+	while cooldown >= 0 do
+		cooldown_GUI.text.Text = cooldown_Value.Value
+		cooldown_GUI.Enabled = true
+
+		task.wait(0.7)
+
+		cooldown_Value.Value = cooldown_Value.Value - 1
+
+		if cooldown_Value.Value == 0 then
+			print("OI!")
+			for _, boxs in ipairs(box) do
+				for _, child in ipairs(boxs:GetChildren()) do
+					if child:IsA("BillboardGui") then
+						local timer_geral = child.timer_Value
+
+						if timer_geral.Value == 0 then
+							local gui_local = timer_geral:FindFirstAncestorOfClass("BillboardGui")
+							print(gui_local)
+						end
+					end
+				end	
+			end
+		end
+	end
+end
+
 local function disableAllPrompts()
     for _, boxs in ipairs(box) do
         for _, child in ipairs(boxs:GetChildren()) do
             if child:IsA("ProximityPrompt") then
-                child.Enabled = false
+				if child.Name == "pegar_Prompt" and isCarrying then
+                	child.Enabled = false
+				end
             end
         end
     end
+end
+
+local function enableAllPrompts()
+    for _, boxs in ipairs(box) do
+        for _, child in ipairs(boxs:GetChildren()) do
+            if child:IsA("ProximityPrompt") then
+				if child.Name == "pegar_Prompt" and not isCarrying then
+                	child.Enabled = true
+				end
+            end
+        end
+    end
+end
+
+function soltar_Box(player)
+	local character = player.Character
+	local humanoid = character:FindFirstChildOfClass("Humanoid")
+
+	local box_local = new_Prompt.Parent
+
+	animTrack:Stop()
+	isCarrying = false
+
+	local weld = character.HumanoidRootPart:FindFirstChild("box_Weld")
+	task.wait(0.3)
+	weld:Destroy()
+
+	if character then
+		function onTouch(hit)
+			if hit:IsA("Part") and hit.Position.Y <= box_local.Position.Y and not hit:IsDescendantOf(character) and box_local and not isCarrying then
+				local box_local = new_Prompt.Parent
+
+				local Timer = box_local.Timer
+	
+				box_local.CanCollide = true
+				box_local.Anchored = true
+
+				task.wait(0.4)
+				enableAllPrompts()
+				new_Prompt:Destroy()
+
+				old_box = box_local
+				cooldown_Box(Timer)
+			end	
+		end
+	end
+	if not isCarrying then
+		box_local.Touched:Connect(onTouch)
+	end
 end
 
 for _, boxs in ipairs(box) do
@@ -25,15 +111,13 @@ for _, boxs in ipairs(box) do
 			function proximityBox(player)
 				isCarrying = true
 				if isCarrying then
-					local character = player.character
+					local character = player.Character
 					local humanoid = character:FindFirstChildOfClass("Humanoid")
 
 					local box = proximity.Parent
 					--configs
 					box.CanCollide = false
 					box.Anchored = false
-
-					local prompt = box.ProximityPrompt
 
 					--anim play
 					animTrack = humanoid:LoadAnimation(animation)
@@ -50,13 +134,16 @@ for _, boxs in ipairs(box) do
 
 					--definir cor
 					box_Color = box:FindFirstChild("box_Color").Value
+
 					disableAllPrompts()
+
+					new_Prompt = Instance.new("ProximityPrompt", box)
+					new_Prompt.Name = "soltar_Prompt"
+					--function
+					new_Prompt.Triggered:Connect(soltar_Box)
 				end
 			end
 		end
 	end
-	boxs:WaitForChild("ProximityPrompt").Triggered:Connect(proximityBox)
+	boxs:WaitForChild("pegar_Prompt").Triggered:Connect(proximityBox)
 end
-	
-
-
